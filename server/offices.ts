30861af49511726811550daeb63c6e0226cb2ed0
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2024-01-20 20:35:49 -0800

    Update offices.ts

--- server/offices.ts
+++ server/offices.ts
@@ -14,4 +14,22 @@ export const OFFICES = {
 		description: "Title IX is a comprehensive federal law that prohibits gender or sex based discrimination in any federally funded education program or activity.\nThe TItle IX Office is committed preventing and protecting campus members from all forms of sex discrimination, such as sexual harrasment, sexual violence, & gender-based harassment and discrimination",
 		services: ["The Title IX Office works preventively to prevent discrimination and harassment. Title IX also receives and responds to reports of misconduct, and will work to remedy and prevent future harm. You can consult a member of the Title IX Office to file a report in person or online, and use the online reporting link to send it through."]
 	}
+
+	"Counseling and Psychological Services (CAPS)": {
+		link: "https://caps.ucsc.edu/",
+		actions: [
+			{
+				name: "Let's Talk Drop-In Services",
+				link: "https://www.google.com/url?q=https%3A%2F%2Fucsc.zoom.us%2Fj%2F7890515689%3Fpwd%3DQitDRG5Ra0pHUEtKZUN5Y0ZDUUhUQT09&sa=D&ust=1706213460000000&usg=AOvVaw1RTvRwvF1dVa2pct_7_2s9",
+				name2: "Mental Health Virtual Toolbox",
+				link2: "https://caps.ucsc.edu/groups-and-workshops/virtual-toolbox-anytime.html"
+			},
+		],
+		contact: {
+			email: "No Email",
+			phone: "831-459-2428",
+		},
+		description: "UCSC students contact CAPS for a variety of reasons, including stress; depression; anxiety; academic problems; problems with friends, roommates, or romantic partners; family issues; life transitions; eating and body image concerns; grief and loss; substance use concerns; issues related to oppression, discrimination, or harassment; personal identity issues; recovery from trauma; suicidal thoughts; and adjusting to the complex demands of university life. More on CAP's About Page",
+		services: ["On-Campus Therapy, Telemental Health, Groups and Workshops, Couples and Family Counseling, Crisis Services"]
+	}
 };
