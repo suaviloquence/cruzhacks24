b6f369eca1ef01268b274ac3130b243d0924262c
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2024-01-20 22:00:42 -0800

    Update offices.ts

--- server/offices.ts
+++ server/offices.ts
@@ -90,4 +90,223 @@ export const OFFICES = {
 		services: ["The ombudsperson helps individuals resolve issues fairly, and if possible, informally. The ombudsperson has been given broad access to information and resources that can be of assistance in developing options to help people resolve their issues. This includes access to University administrators, faculty and staff, and records. The Ombuds acts as a neutral third-party, and will not advocate for either party or make formal decisions, provide legal advice, keep formal documentation, etc. "]
 	},
 
+	"Center for Advocacy, Resources and Empowerment (CARE)": {
+		link: "http://care.ucsc.edu/",
+		actions: [
+			{
+				name: "Seeking Medical Options",
+				phone: "1-888-900-4232",
+			},
+			{
+				name: "Seeking Reporting Options:",
+				phone: "831-459-2462"
+			},
+			
+		],
+		contact: {
+			phone: "(831) 502-CARE (2273)",
+			email: "care@ucsc.edu",
+		},
+		description: "CARE provides free and confidential support to those impacted by sexual assault, intimate partner/domestic violence, stalking, or sexual harassment.",
+		services: ["empty"] //COME BACK
+	},
+
+	"Student Health Outreach and Promotion (SHOP)": {
+		link: "https://shop.ucsc.edu/sexual-health/index.html",
+		actions: [
+			{
+				name: "Health and Wellness Coaching with SHOP Advisors",
+				link: "https://tinyurl.com/SHOPUCSC",
+			},
+			{
+				name: "SHOP Talk Toolkit:",
+				link: "https://drive.google.com/drive/folders/1WU7_UCOtbWv-vSwV8M7FDo0akKADceev?usp=share_link"
+			},
+			
+		],
+		contact: {
+			phone: "(831) 459-3772",
+			studentContactEmail: "shop@ucsc.edu",
+		},
+		description: "SHOP is UCSC’s destination for information, conversation and self-exploration. It’s the place for students to go to learn about health and wellness in a safe, non- judgmental environment. SHOP provides opportunities for students to explore and enhance their health and wellness as they pursue their academic goals. SHOP offers information, education, resources and support on issues related to Alcohol and other Drugs, Sexually Transmitted Infections and Sexual Health, Mental Health and other concerns relevant to college students. All UCSC students are welcome and encouraged to drop in and speak with SHOP’s professional and student staff.",
+		services: ["SHOP Talk: SHOP Talk peer educators are trained to promote mental health resources and share skills that will help students succeed. Topics include: loneliness, imposter syndrome, sleep hygiene, communication, school/life balance, and more", "Health and Wellness Coaching: Wellness refers to the whole human - it is emotional, physical, social & spiritual. During your appointment we can tackle - General Wellness: Managing Stress, Breaking Isolation, Social Media Balance, Daily Gratitude & Joy - Sexual Health: Sex & Communication, Pregnancy Options, Birth Control, STI Prevention - Alcohol and Drugs: Recovery Support, Harm Reduction, Reduced Use, Safer Use Info"
+		]
+	},
+
+	"The Cove": {
+		link: "https://shop.ucsc.edu/students-in-recovery/index.html",
+		actions: [
+			{
+				name: "Weekly workshops and, recovery meetings, and drop-in hours for drug/abuse recovery; sober events on/off campus; provide connections to resource for recovery support; hold students accountable for recovery process",
+				link: "https://shop.ucsc.edu/alcohol-other-drugs/aodappointmentform.html",
+			},
+			
+			
+		],
+		contact: {
+			phone: "(831) 459-1417",
+			
+		},
+		description: "The Cove is an all-inclusive community for students recovering from alcohol and/or other drugs, loved ones of those with addictions, allies, and anyone else who supports recovery. We are a diverse group, varying in ages and lengths of sobriety. The Cove is supported by staff and faculty at the university and community members.",
+		services: ["The Cove: Recovery and support circle for those who have had a past with substance/physical abuse, looking for support in harm recovery, and available for people who wish to support others going through those problems. Provides a chance to socalize and network with others facing similar problems, and mentoring spaces for people seeking guidance."
+		]
+	},
+
+	"Slug Love": {
+		link: "https://shop.ucsc.edu/sexual-health/index.html",
+		actions: [
+			{
+				name: "Online Birth Control by order via general Health and Wellness Coaching session consultation",
+				link: "https://shop.ucsc.edu/wellness-coaching/index.html",
+			},
+			{
+				name: "Condom Co-Op for free safe sex supplies",
+				link: "https://shop.ucsc.edu/sexual-health/condom-co-op.html"
+			},
+			
+		],
+		contact: {
+			instagram: "shop@ucsc.edu",
+		},
+		description: "The cornerstone of our sexual health work at SHOP is our brilliant student peer educators. Highly trained peers work alongside the professional SHOP staff to support students around their reproductive and sexual health. The Slug Love team promote sexual health through educational workshops, fun on-campus events, a safer sex collective known as the Condom Co-Op, Birth Control Patrol (education & support around contraception), and sharing information about DIY Sexually Transmitted Infection testing services",
+		services: ["Slug Love Workshops", "On Campus Events", "Condom Co-Op", "Brith Control Patrol", "DIY Sexually Transmitted Infection testing services"
+		]
+	},
+
+	//COME BACK NOT FINISHED
+	"Student Organization Advising and Resources (SOAR)": {
+		link: "https://soar.ucsc.edu/",
+		actions: [
+			{
+				name: "Seeking Medical Options",
+				phone: "1-888-900-4232",
+			},
+			{
+				name: "Seeking Reporting Options:",
+				phone: "831-459-2462"
+			},
+			
+		],
+		contact: {
+			phone: "(831) 502-CARE (2273)",
+			email: "care@ucsc.edu",
+		},
+		description: "CARE provides free and confidential support to those impacted by sexual assault, intimate partner/domestic violence, stalking, or sexual harassment.",
+		services: ["empty"] //COME BACK
+	},
+
+	//COME BACK NOT FINISHED
+	"SOAR Student Media Cultural Arts and Diversity (SOMeCA)": {
+		link: "https://someca.ucsc.edu/who-we-are/index.html",
+		actions: [
+			{
+				name: "Seeking Medical Options",
+				phone: "1-888-900-4232",
+			},
+			{
+				name: "Seeking Reporting Options:",
+				phone: "831-459-2462"
+			},
+			
+		],
+		contact: {
+			phone: "(831) 502-CARE (2273)",
+			email: "care@ucsc.edu",
+		},
+		description: "CARE provides free and confidential support to those impacted by sexual assault, intimate partner/domestic violence, stalking, or sexual harassment.",
+		services: ["empty"] //COME BACK
+	},
+
+	//COME BACK NOT FINISHED
+	"Ethnic Resource Centers (ERC)": {
+		link: "https://resourcecenters.ucsc.edu/",
+		actions: [
+			{
+				name: "Academic Excellence Program (ACE)",
+				link: "http://ace.ucsc.edu/",
+			},
+			{
+				name: "Chancellor Undergraduate Internship Program (CUIP)",
+				link: "http://intern.ucsc.edu/cuip/"
+			},
+			{
+				name: "Education Abroad Program (EAP)",
+				link: "https://globallearning.ucsc.edu/programs/uceap/"
+			},
+			{
+				name: "Education Opportunity Program (EOP)",
+				link: "http://eop.ucsc.edu/"
+			},
+			{
+				name: "EOP Pre-Graduate Programs",
+				link: "http://eop.ucsc.edu/eop_services/pre-grad/index.html"
+			},
+			{
+				name: "Minority Access to Research Careers/Minority Biomedical Research Support (MARC/MBRS) ",
+				link: "http://stemdiv.ucsc.edu/marc/"
+			},
+			{
+				name: "STEM Diversity Programs ",
+				link: "http://stemdiv.ucsc.edu/"
+			},
+			{
+				name: "The University of California Washington Center (UCDC)",
+				link: "http://politics.ucsc.edu/undergraduate/ucdc/index.html"
+			},
+			
+		],
+		//these ar enot finished
+		contact: {
+			phone: "(831) 502-CARE (2273)",
+			email: "care@ucsc.edu",
+		},
+		description: "CARE provides free and confidential support to those impacted by sexual assault, intimate partner/domestic violence, stalking, or sexual harassment.",
+		services: ["empty"] //COME BACK
+	},
+
+	"African American Resource and Cultural Center (AARCC)": {
+		link: "https://aarcc.ucsc.edu/",
+		actions: [
+			{
+				name: "Schedule Meeting with the Director",
+				link: "https://calendly.com/autumn_aarcc",
+			},
+			{
+				name: "African American Mentorship Program (AAMP) for Frosh and Transfers to help transition into their college lives:",
+				email: "abc.aamp@ucsc.edu"
+			},
+			{
+				name: "Black Academy for first years, hosting workshops to give new students to resources and the black community to help them succeed",
+				link: "https://sites.google.com/ucsc.edu/ucscblackacademyresourcesite/program-information?authuser=0",
+			},
+			{
+				name: "F.R.E.D. (Facilitators for Race and Ethnic Diversity) Consultation",
+				link: "https://docs.google.com/forms/d/12CrZ8xUprPF_rLAkGPWYrslzpzooWVxqcyBv3FcYMFE/edit",
+			},
+			{
+				name: "Arts & Enrichment Programs",
+				link: "https://aarcc.ucsc.edu/get-involved/arts-and-enrichment.html",
+			},
+			{
+				name: "Academic Societies ",
+				link: "https://aarcc.ucsc.edu/get-involved/academic-societies.html",
+			},
+			{
+				name: "Greek Life",
+				link: "https://aarcc.ucsc.edu/get-involved/academic-societies.html",
+			},
+		],
+		contact: {
+			phone: "(831) 459-3561",
+			email: "aarcc@ucsc.edu",
+		},
+		description: "The mission of the African American Resource & Cultural Center (AARCC) is to provide support for the students in the African, Black, and Caribbean (ABC) communities at the University of California at Santa Cruz. We aim to foster an environment and community that supports all folks from the African Diaspora and encourages the pursuit of a deeper understanding of identity and culture as it relates to one’s educational journey. The AARCC provides a wide range of programs, support services, and resources centered around the Black Diasporic experience. The AARCC serves as an advocate for ABC students on an array of campus matters. We encourage and facilitate student involvement and success in campus life, student organizations, academic excellence, and community engagement.",
+		services: ["AARCC Specific Organizations: African American Theater Arts Troupe (AATAT), Afrikan Black Coalition (ABC), Beautiful Black Queers (BBQ), Black Campus Ministries (BCM), Black Men’s Alliance (BMA), Black Student Union (BSU), Black Womxn’s Alliance (BWA), Destination Higher Education, Hip Hop Congress Chapter, Minorities in Anthropology (MIA), National Society of Black Engineers (NSBE), Rosa Parks African-American Themed House (R.PAATH)", "AARCC Arts & Enrichment: African American Theatre Arts Troupe (AATAT), Destination Higher Education (DHE), Engaging Education (e²), Gospel Choir at Porter College, Rainbow Theatre, Umoja", "AARCC Academic Societies and Organizations: National Society of Black Engineers (NSBE), Black Science Network (BSN), National Association of Black Accountants (NABA)", "AARCC Greek Life: Delta Sigma Theta Sorority, Zeta Phi Beta Sorority"] //COME BACK
+	},
+
+	
+
+
+
+
 };
