929d87ae3c17edb2eed1f8628d338c929d7bb2cb
teahibiscus <109654372+teahibiscus@users.noreply.github.com>
2024-01-20 21:00:34 -0800

    Update offices.ts

--- server/offices.ts
+++ server/offices.ts
@@ -13,7 +13,7 @@ export const OFFICES = {
 		},
 		description: "Title IX is a comprehensive federal law that prohibits gender or sex based discrimination in any federally funded education program or activity.\nThe TItle IX Office is committed preventing and protecting campus members from all forms of sex discrimination, such as sexual harrasment, sexual violence, & gender-based harassment and discrimination",
 		services: ["The Title IX Office works preventively to prevent discrimination and harassment. Title IX also receives and responds to reports of misconduct, and will work to remedy and prevent future harm. You can consult a member of the Title IX Office to file a report in person or online, and use the online reporting link to send it through."]
-	}
+	},
 
 	"Counseling and Psychological Services (CAPS)": {
 		link: "https://caps.ucsc.edu/",
@@ -21,15 +21,73 @@ export const OFFICES = {
 			{
 				name: "Let's Talk Drop-In Services",
 				link: "https://www.google.com/url?q=https%3A%2F%2Fucsc.zoom.us%2Fj%2F7890515689%3Fpwd%3DQitDRG5Ra0pHUEtKZUN5Y0ZDUUhUQT09&sa=D&ust=1706213460000000&usg=AOvVaw1RTvRwvF1dVa2pct_7_2s9",
+			},
+			{
 				name2: "Mental Health Virtual Toolbox",
 				link2: "https://caps.ucsc.edu/groups-and-workshops/virtual-toolbox-anytime.html"
 			},
 		],
 		contact: {
-			email: "No Email",
 			phone: "831-459-2428",
 		},
 		description: "UCSC students contact CAPS for a variety of reasons, including stress; depression; anxiety; academic problems; problems with friends, roommates, or romantic partners; family issues; life transitions; eating and body image concerns; grief and loss; substance use concerns; issues related to oppression, discrimination, or harassment; personal identity issues; recovery from trauma; suicidal thoughts; and adjusting to the complex demands of university life. More on CAP's About Page",
 		services: ["On-Campus Therapy, Telemental Health, Groups and Workshops, Couples and Family Counseling, Crisis Services"]
-	}
+	},
+
+	"Student Health Center": {
+		link: "https://healthcenter.ucsc.edu/",
+		actions: [
+			{
+				name: "Schedule a Full Medical Appointment using Health e-Messenger",
+				link: "https://healthcenter.ucsc.edu/information/health-e-messenger-new.html",
+			},
+			{
+				name: "Schedule an Immunization appointment",
+				link: "https://studenthealth.ucsc.edu/"
+			},
+			{
+				name: "Order an STI test kit (Fill out the STI Questionnaire on Health e-Messenger)",
+				link: "https://healthcenter.ucsc.edu/information/health-e-messenger-new.html"
+			},
+			{
+				name: "Get birth control",
+				link: "https://healthcenter.ucsc.edu/services/birthcontrol.html"
+			},
+			{
+				name: "Check Medical Records",
+				link: "https://healthcenter.ucsc.edu/services/health-information-management.html"
+			}
+		],
+		contact: {
+			insuranceEmail: "insure@ucsc.edu",
+			adminEmail: "healthcenter@ucsc.edu",
+			phone: "831-459-2428",
+			appointment: "831-459-2500",
+			nurseAdvice: "831-459-2591"
+		},
+		description: "Student Health Center provides quality health care focused on the particular needs of students. All registered students have access to the Student Health Center regardless of their insurance plan.",
+		services: ["The Student Health Center offers routine medical appointments, same day care, counseling services, psychiatry services, nutritional counseling, health promotion programs, x-ray, and full laboratory and pharmacy services on site. Students can also resolve insurance or health-billing-related concerns."]
+	},
+
+	"Office of the Ombuds": {
+		link: "https://ombuds.ucsc.edu/",
+		actions: [
+			{
+				name: "Undergraduates, graduates, and postdocs needing appointments",
+				link: "https://scheduler.zoom.us/anthony-keen-louie/meeting",
+			},
+			{
+				name: "Staff and Faculty needing appointments",
+				link: "https://ombuds.ucsc.edu/"
+			},
+			
+		],
+		contact: {
+			staffContactEmail: "deacker@ucsc.edu",
+			studentContactEmail: "anthonyk@ucsc.edu",
+		},
+		description: "The Office of the Ombuds at the University of California, Santa Cruz was re-established in 2023 to provide confidential, neutral, informal, and independent dispute resolution and mediation services to members of the UCSC community, predicated on the principles of fairness, equity, and respect. The structure and practice of the office is built on independence, impartiality and confidentiality.",
+		services: ["The ombudsperson helps individuals resolve issues fairly, and if possible, informally. The ombudsperson has been given broad access to information and resources that can be of assistance in developing options to help people resolve their issues. This includes access to University administrators, faculty and staff, and records. The Ombuds acts as a neutral third-party, and will not advocate for either party or make formal decisions, provide legal advice, keep formal documentation, etc. "]
+	},
+
 };
