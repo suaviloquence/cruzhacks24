ace7cdecb11d756c4a4c45e9aa27642d7e7cd546
m <m@mcarr.one>
2024-01-20 20:19:13 -0800

    offices.ts

---
+++ server/offices.ts
@@ -0,0 +1,17 @@
+export const OFFICES = {
+	"Title IX Office": {
+		link: "https://titleix.ucsc.edu/index.html",
+		actions: [
+			{
+				name: "Report On-Campus Discrimination/Harassment",
+				link: "https://uctitleix.i-sight.com/portal"
+			},
+		],
+		contact: {
+			email: "titleix@ucsc.edu",
+			phone: "831-459-2462",
+		},
+		description: "Title IX is a comprehensive federal law that prohibits gender or sex based discrimination in any federally funded education program or activity.\nThe TItle IX Office is committed preventing and protecting campus members from all forms of sex discrimination, such as sexual harrasment, sexual violence, & gender-based harassment and discrimination",
+		services: ["The Title IX Office works preventively to prevent discrimination and harassment. Title IX also receives and responds to reports of misconduct, and will work to remedy and prevent future harm. You can consult a member of the Title IX Office to file a report in person or online, and use the online reporting link to send it through."]
+	}
+};
